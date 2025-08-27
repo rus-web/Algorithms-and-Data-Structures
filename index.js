2a660eb7ada2e3f4d444b0035402ac75517ae7cc
Ruslan <rus.tinaev.web@gmail.com>
2025-08-27 16:39:29 +0300

    % Массивы

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 
-
 // /* Лекиця 1.2 Сложность алгоритмов*/
-
 
 // // Линейная сложность O(n) //
 // function foo1(n){
@@ -144,27 +142,27 @@
 /* В школе прошёл экзамен по математике. Некоторые ученики списали решения, были замечены и получили 0 баллов. Помогите учителю пересортировать оценки учеников. 
 Все оценки, равные 0, должны быть перемещены в конец списка, при этом порядок остальных оценок должен остаться неизменным. */
 
-var N = 6;
-var gradeArr = [0,0,6,0,9,8];
+// var N = 6;
+// var gradeArr = [0,0,6,0,9,8];
 
 //1.3.1
-function moveGrade(N,arr){
-    let notZero = 0;
+// function moveGrade(N,arr){
+//     let notZero = 0;
 
-    for (let i = 0; i < N; i++){
-        if(arr[i] !== 0){
-            arr[notZero] = arr[i];
-            notZero++
-        }
-    };
+//     for (let i = 0; i < N; i++){
+//         if(arr[i] !== 0){
+//             arr[notZero] = arr[i];
+//             notZero++
+//         }
+//     };
 
-    for (let i = notZero; i < N; i++){
-        arr[i] = 0;
-    };
+//     for (let i = notZero; i < N; i++){
+//         arr[i] = 0;
+//     };
 
-    return arr;
+//     return arr;
 
-}
+// }
 // console.log(moveGrade(N,gradeArr))
 
 // 1.3.2
@@ -172,12 +170,58 @@ function moveGrade(N,arr){
 /* Учёные провели ряд экспериментов, но из-за неисправности оборудования некоторые результаты оказались ошибочными. 
 Ошибочные результаты представлены числом element. Ваша задача — удалить все вхождения числа element из массива, сохранив порядок остальных чисел.*/
 
-var n = 7;
-var gradeArr = [1,2,-1,4,5,-1,6,-1];
-var element = -1;
+// var n = 7;
+// var gradeArr = [1,2,-1,4,5,-1,6,-1];
+// var element = -1;
 
-const filter = gradeArr.filter((grade) => grade !== element)
-console.log(filter)
+// const filter = gradeArr.filter((grade) => grade !== element)
+// console.log(filter)
 
+/* Лекиця 1.4 Массивы часть 2*/
 
-/* Лекиця 1.4 Массивы часть 1*/
+// var arr1 = [1,4,6,8,11,120,154,1324]
+// var arr2 = [2,5,7,10,150,156,15678]
+
+// function mergeSortedArrays(arr1,arr2){
+//     mergeArray = [];
+//     i = 0;
+//     j = 0;
+//     while(i < arr1.length || j < arr2.length ){
+//         if (arr1[i] < arr2[j]){
+//             mergeArray.push(arr1[i])
+//             i++;
+//         } else {
+//             mergeArray.push(arr2[j])
+//             j++
+//         }
+            
+//     }
+//     return mergeArray
+// }
+
+// console.log(mergeSortedArrays(arr1,arr2))
+
+// 1.4
+/*Прошла серия беговых соревнований Rock’n’Run. За каждый забег спортсмены получили очки. 
+Ваша задача — найти наибольшее количество очков среди всех результатов.*/
+
+// var N = 5;
+// var runArr = [95,87,100,92,85]
+
+// function bestScore(runArr) {
+//     if (runArr.length === 0) return 0;
+    
+//     let maxScore = runArr[0];
+    
+//     for (let i = 1; i < runArr.length; i++) {
+//         if (runArr[i] > maxScore) {
+//             maxScore = runArr[i];
+//         }
+//     }
+    
+//     return maxScore;
+// }
+// console.log(bestScore(runArr))
+
+/* Лекиця 1.5 Списки*/
+
